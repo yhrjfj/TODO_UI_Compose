1eba4cd654fb04e9eeff4f8adb25515191ca3005
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-01-02 22:36:07 +0600

    Start project

--- app/src/main/java/com/yhr/jfj/todo_ui_compose/ui/theme/Theme.kt
+++ app/src/main/java/com/yhr/jfj/todo_ui_compose/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Purple500,
+    secondary = LightPurple,
+    tertiary = LightBlue
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Purple200,
+    secondary = LightPurple,
+    tertiary = LightBlue
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
