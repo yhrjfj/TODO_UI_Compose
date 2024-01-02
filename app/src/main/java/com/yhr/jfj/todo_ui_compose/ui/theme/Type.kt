621408c63f862cf8dd36c07b942534a7aaaa195f
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-01-02 21:13:43 +0600

    Add custom font

--- app/src/main/java/com/yhr/jfj/todo_ui_compose/ui/theme/Type.kt
+++ app/src/main/java/com/yhr/jfj/todo_ui_compose/ui/theme/Type.kt
@@ -5,6 +5,20 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.yhr.jfj.todo_ui_compose.R
+
+
+val Nunito = FontFamily(
+    listOf(
+        Font(R.font.nunito_light, FontWeight.Light),
+        Font(R.font.nunito_regular, FontWeight.Normal),
+        Font(R.font.nunito_medium, FontWeight.Medium),
+        Font(R.font.nunito_semibold, FontWeight.SemiBold),
+        Font(R.font.nunito_bold, FontWeight.Bold),
+        Font(R.font.nunito_extrabold, FontWeight.ExtraBold)
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
