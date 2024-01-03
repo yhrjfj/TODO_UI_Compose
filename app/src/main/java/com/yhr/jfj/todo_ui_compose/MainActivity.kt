0fd02acdc85d86839d99dc999aa266e3326f07c7
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-01-03 13:54:14 +0600

    Add ProfileHeaderComponent in the MainActivity

--- app/src/main/java/com/yhr/jfj/todo_ui_compose/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/todo_ui_compose/MainActivity.kt
@@ -3,13 +3,10 @@ package com.yhr.jfj.todo_ui_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.ui.unit.dp
+import com.yhr.jfj.todo_ui_compose.taskmanager.data.components.ProfileHeaderComponent
 import com.yhr.jfj.todo_ui_compose.ui.theme.TODO_UI_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +14,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TODO_UI_ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                LazyColumn(
+                    contentPadding = PaddingValues(
+                        top = 16.dp,
+                        start = 16.dp,
+                        bottom = 16.dp
+                    )
+                ){
+                    item {
+                        ProfileHeaderComponent()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TODO_UI_ComposeTheme {
-        Greeting("Android")
     }
 }
