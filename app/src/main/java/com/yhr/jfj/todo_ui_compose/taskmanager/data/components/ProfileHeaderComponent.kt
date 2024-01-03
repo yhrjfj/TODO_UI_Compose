21c7e7702e32c7e6d379c61b8b664c0a2f1cfddd
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-01-03 08:56:45 +0600

    Create header component with picture and badge

---
+++ app/src/main/java/com/yhr/jfj/todo_ui_compose/taskmanager/data/components/ProfileHeaderComponent.kt
@@ -0,0 +1,65 @@
+package com.yhr.jfj.todo_ui_compose.taskmanager.data.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.AlertDialogDefaults.containerColor
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.yhr.jfj.todo_ui_compose.R
+import com.yhr.jfj.todo_ui_compose.ui.theme.Orange
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileHeaderComponent() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Image(
+            painter = painterResource(id = (R.drawable.image)),
+            contentDescription = "Profile",
+            modifier = Modifier
+                .size(30.dp)
+                .clip(CircleShape),
+        )
+        BadgedBox(
+            badge = {
+                Badge {
+                    containerColor
+                }
+            },
+            modifier = Modifier
+                .padding(end = 16.dp)
+        ) {
+            Image(
+                imageVector = Icons.Filled.Notifications,
+                contentDescription = "notification"
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ProfileHeaderComponentPreview() {
+    ProfileHeaderComponent()
+}
